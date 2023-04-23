9e89922a097713a328b988f45b71e8b1bc955229
MattiaSemproli <mattia.semproli2@studio.unibo.it>
2023-04-23 23:31:14 +0200

    Created entity and entity factory interfaces

---
+++ src/main/java/it/unibo/donkeykong/game/model/api/Entity.java
@@ -0,0 +1,48 @@
+package it.unibo.donkeykong.game.model.api;
+
+import java.util.Optional;
+import java.util.Set;
+
+import it.unibo.donkeykong.game.ecs.api.Component;
+import it.unibo.donkeykong.game.ecs.impl.AbstractComponent;
+import it.unibo.donkeykong.utilities.Pair;
+import it.unibo.donkeykong.utilities.Type;
+
+/**
+ * This interface models the structure of an entity
+ */
+public interface Entity {
+
+    /**
+     * @return list of related components
+     */
+    Set<Component> getAllComponents();
+
+    /**
+     * @param <E> type of the component requested
+     * @param classComponent class of the requested component
+     * @return an optional with requested component, an empty if not present
+     */
+    <E extends Component> Optional<E> getComponent(Class<E> classComponent);
+    
+    /**
+     * @param component to add to an entity
+     * @return the entity with the added component
+     */
+    Entity addComponent(AbstractComponent component);
+
+    /**
+     * @return the position of the entity
+     */
+    Pair<Float,Float> getPosition();
+
+    /**
+     * @param position of the entity to be set
+     */
+    void setPosition(Pair<Float,Float> position);
+
+    /**
+     * @return the entity's type
+     */
+    Type getType();
+}
---
+++ src/main/java/it/unibo/donkeykong/game/model/api/EntityFactory.java
@@ -0,0 +1,32 @@
+package it.unibo.donkeykong.game.model.api;
+
+/**
+ * This interface manages the creation of different entities.
+ */
+public interface EntityFactory {
+
+    /**
+     * Create a player
+     */
+    Entity generatePlayer();
+
+    /**
+     * Create a barrel
+     */
+    Entity generateBarrel();
+    
+    /**
+     * Create a monkey
+     */
+    Entity generateMonkey();
+    
+    /**
+     * Create a princess
+     */
+    Entity generatePrincess();
+
+    /**
+     * Create a power up
+     */
+    Entity generatePowerUp();
+}
