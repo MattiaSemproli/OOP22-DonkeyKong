da662d76dd8c0d79161a598b42832fc8ceba9916
MattiaSemproli <mattia.semproli2@studio.unibo.it>
2023-04-23 23:26:52 +0200

    Created Component.java interface and AbstractComponent.java file

---
+++ src/main/java/it/unibo/donkeykong/game/ecs/api/Component.java
@@ -0,0 +1,11 @@
+package it.unibo.donkeykong.game.ecs.api;
+
+/**
+ * Component interface
+ */
+public interface Component {
+    /**
+     * Update component every frame
+     */
+    public void update();
+}
---
+++ src/main/java/it/unibo/donkeykong/game/ecs/impl/AbstractComponent.java
@@ -0,0 +1,10 @@
+package it.unibo.donkeykong.game.ecs.impl;
+
+import it.unibo.donkeykong.game.ecs.api.Component;
+
+/**
+ * Defines the relationship between component and related entity
+ */
+public abstract class AbstractComponent implements Component {
+
+}
