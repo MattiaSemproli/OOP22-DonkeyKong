c8e891b29b00b5011439fe31f6aff46f9859b8e1
MattiaSemproli <mattia.semproli2@studio.unibo.it>
2023-04-23 14:11:43 +0200

    Added Type.java file

---
+++ src/main/java/it/unibo/donkeykong/utilities/Type.java
@@ -0,0 +1,43 @@
+package it.unibo.donkeykong.utilities;
+
+/**
+ * Enum for every existing type.
+ */
+public enum Type {
+    /**
+     * Player
+     */
+    PLAYER,
+    /**
+     * Boss of the level
+     */
+    MONKEY,
+    /**
+     * Princess
+     */
+    PRINCESS,
+    /**
+     * Barrel
+     */
+    BARREL,
+    /**
+     * Block, like roof or floor
+     */
+    BLOCK,
+    /**
+     * Block empty, nothing exist there
+     */
+    BLACK_BLOCK,
+    /**
+     * Ladder
+     */
+    LADDER,
+    /**
+     * Block with ladder
+     */
+    BLOCK_LADDER,
+    /**
+     * Powerup
+     */
+    POWER_UP,
+}
