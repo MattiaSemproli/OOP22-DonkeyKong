e95a7eb34edb262b3bfff0196accf4a7cbe38680
Federico Di Franco <federico.difranco@studio.unibo.it>
2023-04-24 18:06:35 +0200

    Made some changes in Direction.java

--- src/main/java/it/unibo/donkeykong/utilities/Direction.java
+++ src/main/java/it/unibo/donkeykong/utilities/Direction.java
@@ -1,5 +1,27 @@
 package it.unibo.donkeykong.utilities;
 
-public class Direction {
+public enum Direction {
     
+    LEFT(-1,0),
+
+    RIGHT(1,0);
+
+    private int x,y;
+
+    Direction(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public Direction getOppositeDirection() {
+        return this == LEFT ? RIGHT : LEFT;
+    }
 }
