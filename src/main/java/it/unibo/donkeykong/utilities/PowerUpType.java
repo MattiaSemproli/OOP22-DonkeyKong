f682ebc136bead4a5c2336e7399e8ffb111d36ce
Federico Di Franco <federico.difranco@studio.unibo.it>
2023-04-24 18:19:43 +0200

    Added an enum for PowerUpTypes

--- src/main/java/it/unibo/donkeykong/utilities/PowerUpType.java
+++ src/main/java/it/unibo/donkeykong/utilities/PowerUpType.java
@@ -1,5 +1,28 @@
 package it.unibo.donkeykong.utilities;
 
 public enum PowerUpType {
-    
+    /**
+     * Shield
+     */
+    SHIELD,
+    /**
+     * Extra life
+     */
+    HEART,
+    /**
+     * Immortality for few seconds
+     */
+    STAR,
+    /**
+     * Freeze Donkey Kong for few seconds
+     */
+    SNOWFLAKE,
+    /**
+     * Mario takes double damage from barrels
+     */
+    DOUBLE_DAMAGE,
+    /**
+     * Slow for few seconds
+     */
+    SLOW,
 }
