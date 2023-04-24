e917af0186b9059c1d3cd0aa41805990eb764239
Federico Di Franco <federico.difranco@studio.unibo.it>
2023-04-24 16:13:17 +0200

    Added KeyListener methods in KeyboardInputs.java

--- src/main/java/it/unibo/donkeykong/inputs/KeyboardInputs.java
+++ src/main/java/it/unibo/donkeykong/inputs/KeyboardInputs.java
@@ -1,5 +1,30 @@
 package it.unibo.donkeykong.inputs;
 
-public class KeyboardInputs {
+import java.awt.event.KeyListener;
+import it.unibo.donkeykong.view.GamePanel;
+import java.awt.event.KeyEvent;
+
+public final class KeyboardInputs implements KeyListener {
+
+    private final GamePanel dkPanel;
+
+    public KeyboardInputs(final GamePanel dkPanel) {
+        this.dkPanel = dkPanel;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent arg0) {
+        
+    }
+
+    @Override
+    public void keyReleased(KeyEvent arg0) {
+        
+    }
+
+    @Override
+    public void keyTyped(KeyEvent arg0) {
+        
+    }
     
 }
