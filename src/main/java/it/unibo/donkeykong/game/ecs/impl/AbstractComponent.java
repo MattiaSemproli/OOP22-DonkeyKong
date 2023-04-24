ee59813f526dc568c7aa5783ed91771b9972b26d
MattiaSemproli <mattia.semproli2@studio.unibo.it>
2023-04-24 18:24:06 +0200

    Made some changes in Entity.java and implemented AbstractComponent.java

--- src/main/java/it/unibo/donkeykong/game/ecs/impl/AbstractComponent.java
+++ src/main/java/it/unibo/donkeykong/game/ecs/impl/AbstractComponent.java
@@ -1,10 +1,26 @@
 package it.unibo.donkeykong.game.ecs.impl;
 
 import it.unibo.donkeykong.game.ecs.api.Component;
+import it.unibo.donkeykong.game.model.api.Entity;
 
 /**
  * Defines the relationship between component and related entity
  */
 public abstract class AbstractComponent implements Component {
+    
+    private Entity entity;
 
+    /**
+     * @return entity related to the component
+     */
+    public Entity getEntity() {
+        return this.entity;
+    }
+
+    /**
+     * @param entity entity to set to the component.
+     */
+    public void setEntity(final Entity entity) {
+        this.entity = entity;
+    }
 }
