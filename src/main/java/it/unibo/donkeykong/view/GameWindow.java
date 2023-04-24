09a3e4a348e22fd4e65665df3e41dc8396b06b37
MattiaSemproli <mattia.semproli2@studio.unibo.it>
2023-04-24 14:59:20 +0200

    Added game window and panel file

---
+++ src/main/java/it/unibo/donkeykong/view/GameWindow.java
@@ -0,0 +1,30 @@
+package it.unibo.donkeykong.view;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+/**
+ * Game window.
+ */
+public class GameWindow {
+  /**
+   * The window represents "the box" of the game application.
+   * It contains a single panel that displays the game world and other graphical elements.
+   * The settings below configure the appearance and behavior of the window.
+   * 
+   * @param dkPanel The panel containing the content to be displayed in the window.
+   */
+  public GameWindow(final GamePanel dkPanel) {
+    final JFrame jframe = new JFrame();
+    jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    jframe.add(dkPanel);
+    jframe.setTitle("Donkey Kong");
+    jframe.setLocationRelativeTo(null);
+    jframe.setResizable(false);
+    jframe.pack();
+    jframe.setVisible(true);
+    jframe.setFocusable(true);
+    jframe.requestFocus();
+    jframe.setLocationRelativeTo(null);
+  }
+}
