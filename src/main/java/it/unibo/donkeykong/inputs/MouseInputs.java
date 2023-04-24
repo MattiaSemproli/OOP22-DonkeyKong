ba9d09f0eaf55fecb9f868a9a2bfc712a98a9b43
Federico Di Franco <federico.difranco@studio.unibo.it>
2023-04-24 16:17:09 +0200

    Created MouseInputs.java, added MouseListener methods

---
+++ src/main/java/it/unibo/donkeykong/inputs/MouseInputs.java
@@ -0,0 +1,41 @@
+package it.unibo.donkeykong.inputs;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import it.unibo.donkeykong.view.GamePanel;
+
+public class MouseInputs implements MouseListener {
+
+    private final GamePanel dkPanel;
+
+    public MouseInputs(GamePanel dkPanel) {
+        this.dkPanel = dkPanel;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent arg0) {
+        
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent arg0) {
+        
+    }
+
+    @Override
+    public void mouseExited(MouseEvent arg0) {
+        
+    }
+
+    @Override
+    public void mousePressed(MouseEvent arg0) {
+        
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent arg0) {
+        
+    }
+    
+}
