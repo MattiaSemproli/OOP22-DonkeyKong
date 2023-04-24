52ef31cad3e72aae98efc15186556a094846c7be
MattiaSemproli <mattia.semproli2@studio.unibo.it>
2023-04-24 14:53:45 +0200

    Added default size constants

--- src/main/java/it/unibo/donkeykong/utilities/Constants.java
+++ src/main/java/it/unibo/donkeykong/utilities/Constants.java
@@ -1,5 +1,11 @@
 package it.unibo.donkeykong.utilities;
 
 public class Constants {
-    
+    public final static int TILES_DEFAULT_SIZE = 16;
+    public final static float SCALE = 3.0f;
+    public final static int TILES_IN_WIDTH = 14;
+    public final static int TILES_IN_HEIGHT = 14;
+    public final static int SCALED_TILES_SIZE = Math.round(TILES_DEFAULT_SIZE * SCALE);
+    public final static int GAME_WIDTH = SCALED_TILES_SIZE * TILES_IN_WIDTH;
+    public final static int GAME_HEIGHT = SCALED_TILES_SIZE * TILES_IN_HEIGHT;
 }
