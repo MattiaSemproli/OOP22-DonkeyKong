71ef487e1eb49076b3fd002b2e3febbfca9e297d
MattiaSemproli <mattia.semproli2@studio.unibo.it>
2023-04-26 22:25:21 +0200

    Added res folder with some images and created MainMenuView

---
+++ src/main/java/it/unibo/donkeykong/view/MainMenuView.java
@@ -0,0 +1,27 @@
+package it.unibo.donkeykong.view;
+
+import java.awt.Graphics;
+
+import it.unibo.donkeykong.controller.api.GameEngine;
+
+public class MainMenuView implements GameEngine{
+
+    public MainMenuView() {
+
+        loadBackground();
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    private void loadBackground() {
+
+    }
+
+    @Override
+    public void draw(final Graphics g) {
+
+    }
+}
