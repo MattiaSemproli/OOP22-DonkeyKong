1126c31922f4b4f3a907aead9bd563203b3c62d4
mattia semproli <mattia.semproli2@studio.unibo.it>
2023-04-26 09:28:56 +0200

    Fixed javadoc in Gamestate.java file

--- src/main/java/it/unibo/donkeykong/utilities/Gamestate.java
+++ src/main/java/it/unibo/donkeykong/utilities/Gamestate.java
@@ -1,28 +1,55 @@
 package it.unibo.donkeykong.utilities;
 
+/**
+ * Enum representing the state of the game.
+ */
 public enum Gamestate {
+    /**
+     * Currently in the menu.
+     */
     MENU,
-
+    /**
+     * Currently in the settings.
+     */
     SETTINGS,
-
+    /**
+     * Currently choosing the level to play.
+     */
     CHOSING_LEVELS,
-
+    /**
+     * Currently in the shop.
+     */
     SHOP,
-
+    /**
+     * Currently playing.
+     */
     PLAYING,
-
+    /**
+     * Currently in game pause.
+     */
     PAUSE,
-
+    /**
+     * Currently in the win view.
+     */
     WIN,
-
+    /**
+     * Currently in you died (death = lose) view.
+     */
     DEATH;
 
     public static Gamestate gamestate = MENU;
 
+    /**
+     * 
+     * @return the currently state of the game
+     */
     public static Gamestate getGamestate() {
         return gamestate;
     }
 
+    /**
+     * @param gs set the state of the game
+     */
     public static void setGamestate(final Gamestate gs) {
         gamestate = gs;
     }
