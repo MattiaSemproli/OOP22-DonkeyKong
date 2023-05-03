34d95df36f17d2adbd2404800dee040845f371ea
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2023-05-03 15:33:25 +0200

    Fixed checkstyle in api GameEngine

--- src/main/java/it/unibo/donkeykong/controller/api/GameEngine.java
+++ src/main/java/it/unibo/donkeykong/controller/api/GameEngine.java
@@ -2,10 +2,10 @@ package it.unibo.donkeykong.controller.api;
 
 import java.awt.Graphics;
 
-public interface GameEngine{
+public interface GameEngine {
 
-    public void update();
+    void update();
 
-    public void draw(final Graphics g);
+    void draw(Graphics g);
 
 }
