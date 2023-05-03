324026aa1235ff015f5059d3e083c34c4601a2b4
Federico Di Franco <federico.difranco@studio.unibo.it>
2023-05-03 15:23:20 +0200

    Added comments in PowerUpComponent.java and fixed checkstyle

--- src/main/java/it/unibo/donkeykong/game/ecs/impl/PowerUpComponent.java
+++ src/main/java/it/unibo/donkeykong/game/ecs/impl/PowerUpComponent.java
@@ -1,5 +1,29 @@
 package it.unibo.donkeykong.game.ecs.impl;
 
-public class PowerUpComponent {
-    
+import it.unibo.donkeykong.utilities.PowerUpType;
+/**
+ * This component manage all powerUps.
+ */
+public class PowerUpComponent extends AbstractComponent {
+    private final PowerUpType powerUpType;
+
+    /**
+     * This method set the type of powerUp.
+     * 
+     * @param powerUpType type of powerUp
+     */
+    public PowerUpComponent(final PowerUpType powerUpType) {
+        this.powerUpType = powerUpType;
+    }
+
+    @Override
+    public void update() {
+    }
+
+    /**
+     * @return powerup type
+     */
+    public PowerUpType getPowerUpType() {
+        return this.powerUpType;
+    } 
 }
