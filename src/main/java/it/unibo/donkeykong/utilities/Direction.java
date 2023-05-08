68592f3307185cbcbe0bf22ebb66d08e45cc348a
Federico Di Franco <federico.difranco@studio.unibo.it>
2023-05-08 14:12:17 +0200

    Fixed checkstyle errors in MouseInputs.java

--- src/main/java/it/unibo/donkeykong/utilities/Direction.java
+++ src/main/java/it/unibo/donkeykong/utilities/Direction.java
@@ -1,26 +1,48 @@
 package it.unibo.donkeykong.utilities;
 
+/**
+ * Direction enum.
+ */
 public enum Direction {
-    
-    LEFT(-1,0),
+    /**
+     * left direction.
+     */
+    LEFT(-1, 0), 
+    /**
+     * right direction.
+     */
+    RIGHT(1, 0);
 
-    RIGHT(1,0);
+    private int x;
+    private int y;
 
-    private int x,y;
-
+    /**
+     * @param x
+     * @param y
+     */
     Direction(final int x, final int y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * @return x
+     */
     public int getX() {
         return this.x;
     }
 
+    /**
+     * @return y
+     */
     public int getY() {
         return this.y;
     }
 
+    /**
+     * Method that return the opposite direction.
+     * @return left is the actual direction is right and vice versa 
+    */
     public Direction getOppositeDirection() {
         return this == LEFT ? RIGHT : LEFT;
     }
