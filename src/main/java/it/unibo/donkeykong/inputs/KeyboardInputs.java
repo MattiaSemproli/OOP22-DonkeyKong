4257c37500f1d11bbcdb44c5c71377cbf5f2c5f5
Federico Di Franco <federico.difranco@studio.unibo.it>
2023-05-08 14:46:33 +0200

    Fixed again checkstyle errors in KeyboardUnputs.java

--- src/main/java/it/unibo/donkeykong/inputs/KeyboardInputs.java
+++ src/main/java/it/unibo/donkeykong/inputs/KeyboardInputs.java
@@ -4,27 +4,31 @@ import java.awt.event.KeyListener;
 import it.unibo.donkeykong.view.ApplicationPanel;
 import java.awt.event.KeyEvent;
 
+/**
+ * KeyboardInputsImpl class.
+ */
 public final class KeyboardInputs implements KeyListener {
 
     private final ApplicationPanel dkPanel;
 
+    /**
+     * KeyboardInputsImpl constructor.
+     * 
+     * @param dkPanel Panel of application.
+     */
     public KeyboardInputs(final ApplicationPanel dkPanel) {
         this.dkPanel = dkPanel;
     }
 
     @Override
-    public void keyPressed(KeyEvent arg0) {
-        
+    public void keyPressed(final KeyEvent arg0) {
     }
 
     @Override
-    public void keyReleased(KeyEvent arg0) {
-        
+    public void keyReleased(final KeyEvent arg0) {
     }
 
     @Override
-    public void keyTyped(KeyEvent arg0) {
-        
+    public void keyTyped(final KeyEvent arg0) {
     }
-    
 }
