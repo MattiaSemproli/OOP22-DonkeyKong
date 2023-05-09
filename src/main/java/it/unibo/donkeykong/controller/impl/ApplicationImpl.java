a948f6f29658fe02613ebb56e1c9eacb495f7b96
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2023-05-09 10:47:14 +0200

    Added gameController in ApplicationImpl

--- src/main/java/it/unibo/donkeykong/controller/impl/ApplicationImpl.java
+++ src/main/java/it/unibo/donkeykong/controller/impl/ApplicationImpl.java
@@ -12,6 +12,7 @@ public class ApplicationImpl implements Application {
     private GameEngineImpl gameEngine;
     private MainMenuController mainMenuController;
     private SettingsController settingsController;
+    private GameController gameController;
     private Game game;
 
     public ApplicationImpl() {
@@ -34,6 +35,7 @@ public class ApplicationImpl implements Application {
     public void initialize() {
         this.mainMenuController = new MainMenuController();
         this.settingsController = new SettingsController();
+        this.gameController = new GameController();
     }
 
     public MainMenuController getMainMenuController() {
@@ -46,5 +48,9 @@ public class ApplicationImpl implements Application {
 
     public Game getGame() {
         return this.game;
+    }
+
+    public GameController getGameController() {
+        return this.gameController;
     }
 }
