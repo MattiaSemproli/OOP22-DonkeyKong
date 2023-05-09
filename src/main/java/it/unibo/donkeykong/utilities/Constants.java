d84451d991427fa77d01753859e4024d211b05a4
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2023-05-09 10:00:19 +0200

    Fixed ups value in Constants

--- src/main/java/it/unibo/donkeykong/utilities/Constants.java
+++ src/main/java/it/unibo/donkeykong/utilities/Constants.java
@@ -14,7 +14,7 @@ public class Constants {
 
     public static class GameLoop {
         public final static int FPS_SET = 120;
-        public final static int UPS_SET = 200;
+        public final static int UPS_SET = 120;
         public final static double NANOSECOND = 1000000000.0;
         public final static int FRAME_DEFAULT = 0;
         public final static int UPDATES_DEFAULT = 0;
