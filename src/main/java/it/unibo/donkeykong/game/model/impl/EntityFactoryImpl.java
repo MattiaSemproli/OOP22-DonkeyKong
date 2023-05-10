7a4458562053edcf2b34adbad99d646504b8ad2c
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2023-05-10 16:02:55 +0200

    Created EntityFactoryImpl method

---
+++ src/main/java/it/unibo/donkeykong/game/model/impl/EntityFactoryImpl.java
@@ -0,0 +1,32 @@
+package it.unibo.donkeykong.game.model.impl;
+
+import it.unibo.donkeykong.game.model.api.Entity;
+import it.unibo.donkeykong.game.model.api.EntityFactory;
+
+public class EntityFactoryImpl implements EntityFactory {
+
+    @Override
+    public Entity generatePlayer() {
+        return null;
+    }
+
+    @Override
+    public Entity generateBarrel() {
+        return null;
+    }
+
+    @Override
+    public Entity generateMonkey() {
+        return null;
+    }
+
+    @Override
+    public Entity generatePrincess() {
+        return null;
+    }
+
+    @Override
+    public Entity generatePowerUp() {
+        return null;
+    }
+}
