49abbee31f103b8d41d86f3a2e243be284605644
MattiaSemproli <mattia.semproli2@studio.unibo.it>
2023-05-13 16:36:35 +0200

    Made some changes in Level model

--- src/main/java/it/unibo/donkeykong/game/model/api/Level.java
+++ src/main/java/it/unibo/donkeykong/game/model/api/Level.java
@@ -1,7 +1,5 @@
 package it.unibo.donkeykong.game.model.api;
 
 public interface Level {
-    
-    int getSpriteIndex(int x, int y);
 
 }
--- src/main/java/it/unibo/donkeykong/game/model/impl/LevelImpl.java
+++ src/main/java/it/unibo/donkeykong/game/model/impl/LevelImpl.java
@@ -2,17 +2,6 @@ package it.unibo.donkeykong.game.model.impl;
 
 import it.unibo.donkeykong.game.model.api.Level;
 
-public class LevelImpl implements Level{
-
-    private int[][] levelData;
-
-    public LevelImpl(int[][] levelData) {
-        this.levelData = levelData;
-    }
-
-    @Override
-    public int getSpriteIndex(int x, int y) {
-        return levelData[y][x];
-    }
+public class LevelImpl implements Level {
     
 }
