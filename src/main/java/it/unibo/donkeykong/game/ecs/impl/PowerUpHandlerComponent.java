b91f3153ecd9a7a9ad4e4c7670e10b2b32db89d7
Federico Di Franco <federico.difranco@studio.unibo.it>
2023-05-15 19:35:07 +0200

    Added some methods in PowerUpHandlerComponent.java

--- src/main/java/it/unibo/donkeykong/game/ecs/impl/PowerUpHandlerComponent.java
+++ src/main/java/it/unibo/donkeykong/game/ecs/impl/PowerUpHandlerComponent.java
@@ -13,4 +13,41 @@ public class PowerUpHandlerComponent {
     private boolean shield;
     private boolean immunity;
     private boolean freeze;
+
+    /**
+     * This method initializes all powerups.
+     */
+    public PowerUpHandlerComponent() {
+        this.lifes = 3;
+        this.shield = false;
+        this.immunity = false;
+        this.freeze = false;
+    }
+
+    /**
+     * @return actual lifes of player
+     */
+    public int getLifesNumber() {
+        return this.lifes;
+    }
+
+    /**
+     * Set lifesNumber.
+     */
+    public void setLifesNumber() {
+        this.lifes = 3;
+    }
+
+    /**
+     * This method reduce the player's life when a barrel hit the player.
+     */
+    public void hitPlayer() {
+        this.lifes -= 1;
+    }
+
+    /**
+     * This method modifies player's stats.
+     */
+    public void addPowerUp() {
+    }
 }
