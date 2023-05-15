f8936799ac301696cd60cceb75dce7a53199f08d
Federico Di Franco <federico.difranco@studio.unibo.it>
2023-05-15 19:32:17 +0200

    Added some methods in PowerUpHandlerComponent.java

--- src/main/java/it/unibo/donkeykong/game/ecs/impl/PowerUpHandlerComponent.java
+++ src/main/java/it/unibo/donkeykong/game/ecs/impl/PowerUpHandlerComponent.java
@@ -1,5 +1,16 @@
 package it.unibo.donkeykong.game.ecs.impl;
 
+import it.unibo.donkeykong.utilities.PowerUpType;
+import it.unibo.donkeykong.utilities.Constants;
+
+/**
+ * This component handles all powerUp.
+ */
 public class PowerUpHandlerComponent {
-    
+    private int lifes;
+    private int slow;
+    private int doubledmg;
+    private boolean shield;
+    private boolean immunity;
+    private boolean freeze;
 }
