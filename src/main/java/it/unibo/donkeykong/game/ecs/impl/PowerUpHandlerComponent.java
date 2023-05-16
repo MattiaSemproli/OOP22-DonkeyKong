510892a74bd51f56b914418e2c4cb58630ceadce
Federico Di Franco <federico.difranco@studio.unibo.it>
2023-05-16 12:55:37 +0200

    Modified some methods in PowerUpComponent.java

--- src/main/java/it/unibo/donkeykong/game/ecs/impl/PowerUpHandlerComponent.java
+++ src/main/java/it/unibo/donkeykong/game/ecs/impl/PowerUpHandlerComponent.java
@@ -9,7 +9,7 @@ import it.unibo.donkeykong.utilities.Constants;
 public class PowerUpHandlerComponent {
     private int lifes;
     private int slow;
-    private int doubledmg;
+    private boolean doubledmg;
     private boolean shield;
     private boolean immunity;
     private boolean freeze;
@@ -22,6 +22,7 @@ public class PowerUpHandlerComponent {
         this.shield = false;
         this.immunity = false;
         this.freeze = false;
+        this.doubledmg = false;
     }
 
     /**
@@ -34,20 +35,48 @@ public class PowerUpHandlerComponent {
     /**
      * Set lifesNumber.
      */
-    public void setLifesNumber() {
-        this.lifes = 3;
+    public void setLifesNumber(final int lifes) {
+        this.lifes = lifes;
     }
 
     /**
      * This method reduce the player's life when a barrel hit the player.
      */
-    public void hitPlayer() {
-        this.lifes -= 1;
+    public void hitPlayer(final boolean shield, final boolean immunity, final boolean doubledmg) {
+        if (!this.shield && !this.immunity) {
+            this.lifes -= 1;
+        }
+        if (this.doubledmg) {
+            this.lifes -= 1;
+        }
     }
 
     /**
      * This method modifies player's stats.
      */
-    public void addPowerUp() {
+    public void addPowerUp(final PowerUpType powerUpType) {
+        switch (powerUpType) {
+            case SHIELD:
+                this.shield = true;
+                break;
+            case HEART:
+                if (getLifesNumber() < 3 && getLifesNumber() >= 1) {
+                    setLifesNumber(getLifesNumber() + 1);
+                } 
+                break;
+            case STAR:
+                this.immunity = true;
+                break;
+            case SNOWFLAKE:
+                this.freeze = true;
+                break;
+            case DOUBLE_DAMAGE:
+                this.doubledmg = true;
+                break;
+            case SLOW:
+                break;
+            default:
+                break;
+        }
     }
 }
