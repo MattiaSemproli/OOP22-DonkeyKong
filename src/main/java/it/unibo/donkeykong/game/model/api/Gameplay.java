5e29395de5b7890e31788b440249da46922bf31a
Federico Di Franco <federico.difranco@studio.unibo.it>
2023-05-17 19:34:51 +0200

    Made changes in Gameplay interface and its impl

--- src/main/java/it/unibo/donkeykong/game/model/api/Gameplay.java
+++ src/main/java/it/unibo/donkeykong/game/model/api/Gameplay.java
@@ -1,5 +1,12 @@
 package it.unibo.donkeykong.game.model.api;
 
+import java.util.ArrayList;
+
+import it.unibo.donkeykong.controller.impl.GameController;
+
 public interface Gameplay {
     
+    ArrayList<Entity> getEntities();
+
+    GameController getController();
 }
--- src/main/java/it/unibo/donkeykong/game/model/impl/GameplayImpl.java
+++ src/main/java/it/unibo/donkeykong/game/model/impl/GameplayImpl.java
@@ -1,13 +1,20 @@
 package it.unibo.donkeykong.game.model.impl;
 
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 import it.unibo.donkeykong.controller.api.GameEngine;
+import it.unibo.donkeykong.controller.impl.GameController;
+import it.unibo.donkeykong.game.model.api.Entity;
 import it.unibo.donkeykong.game.model.api.Gameplay;
 
 public class GameplayImpl implements Gameplay, GameEngine {
 
-    public GameplayImpl() {
+    private final GameController controller;
+    private ArrayList<Entity> entities = new ArrayList<>();
+
+    public GameplayImpl(final GameController controller) {
+        this.controller = controller;
         this.initializeGame();
     }
 
@@ -23,5 +30,15 @@ public class GameplayImpl implements Gameplay, GameEngine {
 
     @Override
     public void draw(Graphics g) {
+    }
+
+    @Override
+    public ArrayList<Entity> getEntities() {
+        return this.entities;
+    }
+
+    @Override
+    public GameController getController() {
+        return this.controller;
     }
 }
