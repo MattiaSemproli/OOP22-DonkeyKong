5bb728d0f327c320472af3ede118dd7435807d46
Federico Di Franco <federico.difranco@studio.unibo.it>
2023-05-17 19:37:34 +0200

    Added move method in MovementComponent

--- src/main/java/it/unibo/donkeykong/game/ecs/impl/MovementComponent.java
+++ src/main/java/it/unibo/donkeykong/game/ecs/impl/MovementComponent.java
@@ -1,8 +1,13 @@
 package it.unibo.donkeykong.game.ecs.impl;
 
+import it.unibo.donkeykong.utilities.Direction;
+
 public class MovementComponent extends AbstractComponent {
 
     @Override
     public void update() {
+    }
+
+    public final void moveEntity(final Direction direction) {
     }
 }
