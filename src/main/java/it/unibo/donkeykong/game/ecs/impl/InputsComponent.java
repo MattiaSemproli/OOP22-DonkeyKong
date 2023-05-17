6c5f73179d7dc8a6aa187061eb7ce91024e8425f
Federico Di Franco <federico.difranco@studio.unibo.it>
2023-05-17 19:37:55 +0200

    Implemented inputs component

--- src/main/java/it/unibo/donkeykong/game/ecs/impl/InputsComponent.java
+++ src/main/java/it/unibo/donkeykong/game/ecs/impl/InputsComponent.java
@@ -1,12 +1,36 @@
 package it.unibo.donkeykong.game.ecs.impl;
 
 import java.awt.event.KeyEvent;
+import java.util.Optional;
+
+import it.unibo.donkeykong.utilities.Direction;
 
 public class InputsComponent extends AbstractComponent {
 
-    private KeyEvent input;
+    private Optional<KeyEvent> input;
 
     @Override
     public void update() {
-    }    
+        this.input = this.getEntity().getGameplay().getController().getInputs().stream().findFirst();
+        if (this.input.isPresent()) {
+            switch (this.input.get().getKeyCode()) {
+                case KeyEvent.VK_A:
+                    this.move(Direction.LEFT);
+                    break;
+                case KeyEvent.VK_D:
+                    this.move(Direction.RIGHT);
+                    break;
+                default:
+                    //jump to handle 
+                    break;
+            }
+        }
+    }
+    
+    private void move(final Direction direction) {
+        final Optional<MovementComponent> moveOptional = this.getEntity().getComponent(MovementComponent.class);
+        if (moveOptional.isPresent()) {
+            moveOptional.get().moveEntity(direction);
+        } 
+    }
 }
