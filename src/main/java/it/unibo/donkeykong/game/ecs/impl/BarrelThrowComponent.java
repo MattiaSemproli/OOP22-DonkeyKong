b8690b55a78b33902311c6f15bb33b22df832fb7
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2023-05-17 16:21:20 +0200

    Changed some Components classes

---
+++ src/main/java/it/unibo/donkeykong/game/ecs/impl/BarrelThrowComponent.java
@@ -0,0 +1,15 @@
+package it.unibo.donkeykong.game.ecs.impl;
+
+import it.unibo.donkeykong.utilities.Direction;
+import it.unibo.donkeykong.utilities.Pair;
+import it.unibo.donkeykong.utilities.Constants.Barrel;
+
+public class BarrelThrowComponent extends AbstractComponent {
+
+    public BarrelThrowComponent() {
+    }
+
+    @Override
+    public void update() {
+    }
+}
