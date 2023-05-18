e6c2d61a4187cb8b5708b7a11c63a5f9f20b394e
Federico Di Franco <federico.difranco@studio.unibo.it>
2023-05-18 16:50:06 +0200

    Fixed checkstyle in PowerUpHandlerComponent.java

--- src/main/java/it/unibo/donkeykong/game/ecs/impl/PowerUpHandlerComponent.java
+++ src/main/java/it/unibo/donkeykong/game/ecs/impl/PowerUpHandlerComponent.java
@@ -5,12 +5,10 @@ import it.unibo.donkeykong.utilities.PowerUpType;
 import java.util.ArrayList;
 import java.util.List;
 
-import it.unibo.donkeykong.utilities.Constants;
-
 /**
  * This component handles all powerUp.
  */
-public class PowerUpHandlerComponent extends AbstractComponent{
+public class PowerUpHandlerComponent extends AbstractComponent {
     private int lifes;
     private int slow;
     private boolean shield;
@@ -36,6 +34,7 @@ public class PowerUpHandlerComponent extends AbstractComponent{
 
     /**
      * Set lifesNumber.
+     * @param lifes the number of lives to set
      */
     public void setLifesNumber(final int lifes) {
         if (getLifesNumber() < 3 && getLifesNumber() >= 1) {
@@ -52,17 +51,21 @@ public class PowerUpHandlerComponent extends AbstractComponent{
 
     /**
      * This method reduce the player's life when a barrel hit the player.
+     * @param doubledmg indicates if the damage is doubled
      */
     public void hitPlayer(final boolean doubledmg) {
         if (!this.shield && !this.immunity) {
             this.lifes -= doubledmg ? 2 : 1;
-        }
-        else if(this.shield) {
+        } else if (this.shield) {
             this.lifes -= doubledmg ? 1 : 0;
             this.shield = false;
         }
     }
 
+    /**
+     * This method retrieves the status of the player's power-ups.
+     * @return a list of integers representing the status of each power-up
+     */
     public List<Integer> getStatusPowerUp() {
         return new ArrayList<>() {{
             add(lifes);
@@ -74,6 +77,7 @@ public class PowerUpHandlerComponent extends AbstractComponent{
 
     /**
      * This method modifies player's stats.
+     * @param powerUpType powerUpType the type of powerup to apply to the player
      */
     public void addPowerUp(final PowerUpType powerUpType) {
         switch (powerUpType) {
