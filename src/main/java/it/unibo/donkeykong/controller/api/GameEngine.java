bda93198698d65a0dc7df99a64b7f1d09e477d9a
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2023-05-19 11:34:39 +0200

    Fixed checkstyle errors in GameEngine

--- src/main/java/it/unibo/donkeykong/controller/api/GameEngine.java
+++ src/main/java/it/unibo/donkeykong/controller/api/GameEngine.java
@@ -2,10 +2,21 @@ package it.unibo.donkeykong.controller.api;
 
 import java.awt.Graphics;
 
+/**
+ * GameEngine interface, models gameloop.
+ */
 public interface GameEngine {
 
+    /**
+     * Updating method.
+     */
     void update();
 
+    /**
+     * Drawing method.
+     * 
+     * @param g graphic linked to GameEngine.
+     */
     void draw(Graphics g);
 
 }
