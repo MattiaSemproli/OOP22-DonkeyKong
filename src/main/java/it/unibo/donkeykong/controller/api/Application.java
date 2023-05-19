6d8d9d4f74ba9ac00c1edfab5c0c344ff50a56c2
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2023-05-19 11:42:10 +0200

    Fixed checkstyle errors in Application

--- src/main/java/it/unibo/donkeykong/controller/api/Application.java
+++ src/main/java/it/unibo/donkeykong/controller/api/Application.java
@@ -8,21 +8,63 @@ import it.unibo.donkeykong.controller.impl.PauseController;
 import it.unibo.donkeykong.controller.impl.SettingsController;
 import it.unibo.donkeykong.game.model.impl.Game;
 
+/**
+ * Application class, models controllers management.
+ */
 public interface Application {
 
+    /**
+     * Get GameEngine.
+     * 
+     * @return linked GameEngine.
+     */
     GameEngineImpl getGameEngine();
 
+    /**
+     * Initialize controllers.
+     */
     void initialize();
-    
+
+    /**
+     * Get MainMenuController.
+     * 
+     * @return linked MainMenuController.
+     */
     MainMenuController getMainMenuController();
 
+    /**
+     * Get SettingsController.
+     * 
+     * @return linked SettingsController.
+     */
     SettingsController getSettingsController();
 
+    /**
+     * Get GameController.
+     * 
+     * @return linked GameController.
+     */
     GameController getGameController();
 
+    /**
+     * Get PauseController.
+     * 
+     * @return linked PauseController.
+     */
     PauseController getPauseController();
 
+    /**
+     * Get LevelsMenuController.
+     * 
+     * @return linked LevelsMenuController.
+     */
     LevelsMenuController getLevelsMenuController();
 
+    /**
+     * Get Game.
+     * 
+     * @return linked Game.
+     */
     Game getGame();
+
 }
