ffa5e5953aec7ce6754e1167b7daac8882a40db0
Federico Di Franco <federico.difranco@studio.unibo.it>
2023-05-24 19:12:41 +0200

    Implemented DoubleDamageComponent

--- src/main/java/it/unibo/donkeykong/game/ecs/impl/DoubleDamageComponent.java
+++ src/main/java/it/unibo/donkeykong/game/ecs/impl/DoubleDamageComponent.java
@@ -2,8 +2,21 @@ package it.unibo.donkeykong.game.ecs.impl;
 
 public class DoubleDamageComponent extends AbstractComponent {
 
+    private boolean isDoubleDamage;
+
+    public DoubleDamageComponent() {
+        this.isDoubleDamage = false;
+    }
+
     @Override
     public void update() {
     }
-    
+
+    public final boolean getDoubleDamage() {
+        return this.isDoubleDamage;
+    }
+
+    public final void setDoubleDamage(final boolean isDoubleDamage) {
+        this.isDoubleDamage = isDoubleDamage;
+    }    
 }
