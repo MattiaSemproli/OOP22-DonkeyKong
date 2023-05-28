7cf4e15edf853ae20cec75a825ded6df28ffe5f4
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2023-05-28 21:49:54 +0200

    Modified barrel physics and collision

--- src/main/java/it/unibo/donkeykong/game/ecs/impl/CollisionComponent.java
+++ src/main/java/it/unibo/donkeykong/game/ecs/impl/CollisionComponent.java
@@ -1,8 +1,5 @@
 package it.unibo.donkeykong.game.ecs.impl;
 
-import static it.unibo.donkeykong.utilities.Constants.Level.ladderPadding;
-import static it.unibo.donkeykong.utilities.Constants.Player.canLadderError;
-
 import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
 import java.util.Optional;
@@ -27,13 +24,28 @@ public class CollisionComponent extends AbstractComponent {
     private float x, y;
     private int width, height;
     private Entity entity;
+    private Type eType;
     private Rectangle2D.Float hitbox;
     private Optional<Pair<Float, Float>> nextPosition = Optional.empty();
     private final Random random = new Random();
 
     @Override
     public final void update() {
-
+        this.nextPosition = this.getEntity().getNextPosition();
+        this.entity = this.getEntity();
+        this.eType = this.getEntity().getEntityType();
+        if (this.nextPosition.isPresent()) {
+            this.hitbox.x = nextPosition.get().getX();
+            this.hitbox.y = nextPosition.get().getY();
+            if (eType == Type.BARREL) {
+                this.checkBarrelInAir();
+                this.checkBarrelCollision();
+            }
+        } else {
+            this.nextPosition = Optional.of(this.getEntity().getPosition());
+            this.hitbox.x = this.getEntity().getPosition().getX();
+            this.hitbox.y = this.getEntity().getPosition().getY();
+        }
     }
 
     /**
@@ -58,6 +70,57 @@ public class CollisionComponent extends AbstractComponent {
     public final Rectangle2D.Float getHitbox() {
         return new Rectangle2D.Float(this.hitbox.x, this.hitbox.y, this.hitbox.width, this.hitbox.height);
     }
+
+    private boolean checkIsNotBlock(final Type type) {
+        return type != Type.BLOCK
+            && type != Type.BLOCK_LADDER_DOWN
+            && type != Type.BLOCK_LADDER_UP
+            && type != Type.BLOCK_LADDER_UPDOWN;
+    }
+
+    private void checkBarrelInAir() {
+        if (entity.getGameplay().getEntities()
+                                .stream().filter(e -> !this.checkIsNotBlock(e.getEntityType()))
+                                .anyMatch(e -> {
+                                    Rectangle2D.Float e2hitbox = e.getComponent(CollisionComponent.class).get().getHitbox();
+                                    if (hitbox.intersectsLine(new Line2D.Float(e2hitbox.x,
+                                                                               e2hitbox.y,
+                                                                               e2hitbox.x + e2hitbox.width,
+                                                                               e2hitbox.y))
+                                        && hitbox.y + hitbox.height < e2hitbox.y + 4) {
+                                            return true;
+                                        }
+                                        return false;
+                                })) {
+                                entity.getComponent(MovementComponent.class).get().resetIsInAir();
+                            }  else {
+                                entity.getComponent(MovementComponent.class).get().setIsInAir(true);
+                                this.barrelChangedDirection = false;
+                            }
+    }
+
+    private void checkBarrelCollision() {
+        final MovementComponent mc = this.entity.getComponent(MovementComponent.class).get();
+        this.entity.getGameplay().getEntities().stream()
+                        .filter(e -> !this.checkIsNotBlock(e.getEntityType()))
+                        .filter(e -> hitbox.intersects(e.getComponent(CollisionComponent.class).get().getHitbox()))
+                        .forEach(e -> {
+                            if (random.nextInt(Barrel.totalDirProbability) == Barrel.changeDirProbability
+                                && !this.barrelChangedDirection
+                                && !mc.isInAir()
+                            ) {
+                                mc.changeDirection();
+                            }
+                            this.barrelChangedDirection = true;
+                        });
+        if (hitbox.x < 0 || hitbox.x + hitbox.width > Window.GAME_WIDTH) {
+            mc.moveEntity(mc.getFacing().getOppositeDirection());
+        } else if (hitbox.y > Window.GAME_HEIGHT) {
+            this.entity.getGameplay().removeEntity(entity);
+        } else {
+            entity.setPosition(new Pair<>(this.nextPosition.get().getX(), this.nextPosition.get().getY()));
+        }
+   }
 
     private void initDifferentHitbox(final Type type) {
         width = Constants.Window.SCALED_TILES_SIZE;
